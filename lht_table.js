6099989bad156220367b3f429361b691a4b2b105
Yelaina <ybuford201@west-mec.org>
2020-02-03 18:05:00 -0700

    added captions

--- lht_table.js
+++ lht_table.js
@@ -10,17 +10,20 @@
 
 	
 */
+//adds the starting date and that next 14 days of the table 
 var thisDay = new Date("August 30, 2018");
 var tableHTML = "<table id='eventTable'>";
 tableHTML += "<caption>Upcoming Events</caption>";
 tableHTML += "<tr><th>Date</th><th>Event</th><th>Price</th></tr>";
 var endDate = new Date(thisDay.getTime() + 14 * 24 * 60 * 60 * 1000);
 
+//changes the events as the date changes 
 for(var i = 0; i < eventDates.length; i++){
    var eventDate = new Date(eventDates[i]);
    var eventDay = eventDate.toDateString();
    var eventTime = eventDate.toLocaleTimeString();
 
+//displays the event info in the table row
    if(thisDay <= eventDate && eventDate <= endDate){
       tableHTML += "<tr>";
       tableHTML += "<td>" + eventDay + "@" + eventTime + "</td>";
@@ -30,5 +33,6 @@ for(var i = 0; i < eventDates.length; i++){
    }
 
 }
+
    tableHTML += "</table>";
  document.getElementById("eventList").innerHTML = tableHTML;
